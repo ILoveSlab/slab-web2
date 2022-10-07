54b45c5c8c969ab2941ded3187e320e23175654a
ILoveSlab <nunkeun0@naver.com>
2022-10-07 17:39:41 +0900

    ..

---
+++ src/main/java/com/slab/slabweb/SlabWeb2Application.java
@@ -0,0 +1,13 @@
+package com.slab.slabweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlabWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlabWeb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/slab/slabweb/SlabWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slab.slabweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlabWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
